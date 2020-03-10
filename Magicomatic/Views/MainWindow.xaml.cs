ce2b25f96b39a8fa275cc7dfdddf68aa38390447
Piotr K <navrazz@gmail.com>
2020-03-10 12:37:49 +0100

    Add project files.

---
+++ Magicomatic/Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MahApps.Metro.Controls;
+
+namespace Magicomatic.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Modules/Magicomatic.Modules.Collection/CollectionModule.cs
@@ -0,0 +1,20 @@
+﻿using Magicomatic.Modules.Collection.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace Magicomatic.Modules.Collection
+{
+    public class CollectionModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+ 
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
